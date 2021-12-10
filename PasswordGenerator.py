398cb47fc4b4b5f6dacaafd979a81a860e868916
Reza <99068159@mydavinci.nl>
2021-12-10 12:15:18 +0100

    heb pas gen toegevoegd en heb de helft gedaan denk ik zo

---
+++ PasswordGenerator.py
@@ -0,0 +1,46 @@
+# Maak een programma die een wachtwoord van 24 tekens genereerd.
+
+# Het wachtwoord moet aan de volgende eisen voldoen:
+
+# 2 tot 6 hoofdletters.
+# Minimaal 8 kleine letters.
+# 3 speciale tekens uit de volgende reeks: @ # $ % & _ ?.
+# De speciale tekens mogen niet op de eerste of laatste positie staan en ook niet op een vaste plek.
+# 4 tot 7 cijfers (0 t/m 9).
+# Op de eerste 3 posities mag geen cijfer staan.
+
+import string
+import random
+
+string.ascii_uppercase
+
+HoofdLetterList = []
+for x in range(random.randint(1,6)):
+    HoofdLetter = (random.choice(string.ascii_uppercase))
+    HoofdLetterList.append(HoofdLetter)
+
+
+
+string.ascii_lowercase
+
+KleineLetterList = []
+for y in range(random.randint(1,8)):
+    KleineLetter = (random.choice(string.ascii_lowercase))
+    KleineLetterList.append(KleineLetter)
+
+
+SpecialeTekens = ["@", "#", "$", "%", "&", "_", "?"]
+SpecialeTekenList = []
+for z in range(random.randint(3,10)): #het maximum is niet aangegeven
+    SpeciaalTeken = (random.choice(SpecialeTekens))
+    SpecialeTekenList.append(SpeciaalTeken)
+
+
+
+
+
+
+
+print(HoofdLetterList)
+print(KleineLetterList)
+print(SpecialeTekenList)
